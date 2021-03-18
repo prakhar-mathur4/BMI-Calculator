51f9ca67a0e0ab68d7c08cc9a4e9885946d537e0
Prakhar Mathur <63247633+prakhar-mathur4@users.noreply.github.com>
2021-03-18 12:15:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ Height=float(input("Enter your height in centimeters: "))
 Weight=float(input("Enter your Weight in Kg: "))
 Height = Height/100
 BMI=Weight/(Height*Height)
-print("your Body Mass Index is: ",BMI)
+print("Your Body Mass Index is: ",BMI)
 if(BMI>0):
 	if(BMI<=16):
 		print("you are severely underweight")
